97cf27a02b4206e2570fa653da40c498f23e7f1c
mayushii21 <122178787+mayushii21@users.noreply.github.com>
2023-11-14 10:56:01 +0300

    added brands, adjusted width, enabled sticky top

--- src/vacclean_reports/layout.py
+++ src/vacclean_reports/layout.py
@@ -2,6 +2,7 @@ import dash_bootstrap_components as dbc
 from dash import html
 
 from vacclean_reports.app import app
+from vacclean_reports.components.brands import top_brands_chart
 from vacclean_reports.components.main_chart import (
     agg_dd,
     element_dd,
@@ -29,11 +30,8 @@ app.layout = dbc.Container(
             ],
             align="center",
             # justify="between",
-            style={
-                "position": "-webkit-sticky",
-                # "position": "sticky",
-                "top": 0,
-            },
+            style={"position": "sticky", "top": 0, "zIndex": 100},
+            className="bg-body",  # remove transparency of row
         ),
         dbc.Row(dbc.Col(main_chart())),
         dbc.Row(dbc.Col(top_items_chart())),
@@ -52,10 +50,11 @@ app.layout = dbc.Container(
         ),
         dbc.Row(
             [
-                dbc.Col(top_sellers_n_sku_chart(), width=8),
-                dbc.Col(top_sellers_chart(), width=4),
+                dbc.Col(top_sellers_n_sku_chart(), width=6),
+                dbc.Col(top_sellers_chart(), width=6),
             ]
         ),
+        dbc.Row(dbc.Col(top_brands_chart())),
         dbc.Row(
             dbc.Col(theme_changer, width=2),
         ),
