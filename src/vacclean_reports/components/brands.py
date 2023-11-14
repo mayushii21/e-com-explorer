62b07f8cb9db44b159dfcfa8e135ab478b6ce928
mayushii21 <122178787+mayushii21@users.noreply.github.com>
2023-11-14 10:52:48 +0300

    added brands

---
+++ src/vacclean_reports/components/brands.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import plotly.express as px
+from dash import dcc
+from dash.dependencies import Input, Output
+from dash_bootstrap_templates import ThemeChangerAIO, template_from_url
+
+from vacclean_reports.components.decorators import callback, data_access
+
+
+def top_brands_chart():
+    return dcc.Graph(
+        id="top-brands-chart",
+        # config={"modeBarButtonsToRemove": ["select2d", "lasso2d", "zoom"]},
+        responsive=True,
+        style={"height": "90vh"},
+    )
+
+
+# Update top brand chart
+@callback(
+    Output("top-brands-chart", "figure"),
+    Input("metric-radio", "value"),
+    Input("agg-dd", "value"),
+    Input(ThemeChangerAIO.ids.radio("theme"), "value"),
+)
+@data_access
+def update_brand_chart(
+    df,
+    metric,
+    agg_m,
+    theme,
+):
+    # # Handle double metric
+    # metrics = metric.split()
+    # # Apply proper formatting
+    # main_data = (
+    #     metric + "/день" if len(metrics) == 1 else [m + "/день" for m in metrics]
+    # )
+
+    # Group by brand and month
+    prep = df.groupby(["Бренд", pd.Grouper(key="Дата", freq="M")], as_index=False)[
+        "Выручка/день"
+    ].mean()
+    # Pivot months to columns
+    prep = prep.pivot(index="Бренд", columns="Дата", values="Выручка/день")
+    # Rename months
+    months = ["Aug.", "Sept.", "Oct."]
+    prep.columns = months
+    # Calculate and sort by total
+    prep["Total"] = prep.mean(axis=1)
+    prep.sort_values(by="Total", ascending=False, inplace=True)
+    prep.reset_index(inplace=True)
+    # Plot
+    fig = px.bar(
+        prep[prep.Total != 0],
+        x="Бренд",
+        y=months,
+        labels={"value": "Выручка", "x": "SKU"},
+        title="Ср. выручка/день по брендам и месяцам",
+        barmode="overlay",
+        template=template_from_url(theme),
+    )
+    fig.update_layout(legend_title="Months")
+
+    return fig
